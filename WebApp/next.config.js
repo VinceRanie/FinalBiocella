88cb0f17050a8963e52ef9adb4e47ecc2660299a
VinceRanie <22102959@usc.edu.ph>
2026-05-30 14:40:33 +0800

    Use JavaScript Next config for WebApp

--- WebApp/next.config.js
+++ WebApp/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
@@ -41,4 +40,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
